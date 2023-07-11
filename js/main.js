b739d9cfbdadb9c362bc833e7845e3e6c674899e
VladShekhovtsov <vlad.shekhovtsov93@gmail.com>
2023-07-11 16:11:51 +0300

    start_commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        prevArrow: '<div class="slider-arrow slider-prev"><i class="fa-solid fa-chevron-left"></i></div>',
+        nextArrow: '<div class="slider-arrow slider-next"><i class="fa-solid fa-chevron-right"></i></div>',
+        dots: false,
+        adaptiveHeight: true,
+    });
+});
